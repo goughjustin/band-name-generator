27cc5ac27635052b5b35c903dc599fdb457dee84
Justin <gough.justin@gmail.com>
2015-06-23 10:15:28 -0700

    fixed jscs and jshint issues

--- app/script.js
+++ app/script.js
@@ -1,65 +1,63 @@
-"use strict";
+'use strict';
 
 $(function() {
 
-  $("#name").on("click", function() {
-    $.get("/adjective", function(response) {
+  $('#name').on('click', function() {
+    $.get('/adjective', function(response) {
       var adjective = response.word;
-      $("#adjective").text(adjective);
-  });
-  });
-
-  $("#name").on("click", function() {
-    $.get("/verb", function(response) {
-      var verb = response.word;
-      $("#verb").text(verb);
-  });
-  });
-
-  $("#name").on("click", function() {
-    $.get("/noun", function(response) {
-      var noun = response.word;
-      $("#noun").text(noun);
+      $('#adjective').text(adjective);
     });
   });
 
- $("#submitWords").on("submit", function(e) {
+  $('#name').on('click', function() {
+    $.get('/verb', function(response) {
+      var verb = response.word;
+      $('#verb').text(verb);
+    });
+  });
+
+  $('#name').on('click', function() {
+    $.get('/noun', function(response) {
+      var noun = response.word;
+      $('#noun').text(noun);
+    });
+  });
+
+  $('#submitWords').on('submit', function(e) {
     e.preventDefault();
 
-    var adjective = $("input[name=adjective]").val();
+    var adjective = $('input[name=adjective]').val();
     var adjPost;
 
     if (adjective) {
       adjPost = {word: adjective};
-      $.post("adjective", adjPost, function(response) {
+      $.post('adjective', adjPost, function(response) {
         var adjectiveRes = response.msg;
-        $("#adjectiveRes").text(adjectiveRes);
+        $('#adjectiveRes').text(adjectiveRes);
       });
     }
 
-    var verb = $("input[name=verb]").val();
+    var verb = $('input[name=verb]').val();
     var verbPost;
 
     if (verb) {
       verbPost = {word: verb};
-      $.post("verb", verbPost, function(response) {
+      $.post('verb', verbPost, function(response) {
         var verbRes = response.msg;
-        $("#verbRes").text(verbRes);
+        $('#verbRes').text(verbRes);
       });
     }
 
-    var noun = $("input[name=noun]").val();
+    var noun = $('input[name=noun]').val();
     var nounPost;
 
     if (noun) {
       nounPost = {word: noun};
-      $.post("noun", nounPost, function(response) {
+      $.post('noun', nounPost, function(response) {
         var nounRes = response.msg;
-        $("#nounRes").text(nounRes);
+        $('#nounRes').text(nounRes);
       });
     }
-
-
 
   });
 });
--- server.js
+++ server.js
@@ -9,42 +9,28 @@ var getRandomWord = require('./lib/getrandomword.js');
 var postRandomWord = require('./lib/postrandomword.js');
 var app = express();
 var port = process.env.PORT || 3000;
-//create .js file for Verb, Noun, postRandomword
 
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({extended: true}));
-
 app.use(express.static(__dirname + '/app/'));
-
-
-
-
-
-
-
 
 var adjective = new Adjective();
 var verb = new Verb();
 var noun = new Noun();
 
-
-
-
-
-
 app.get('/', function(req, res) {
   res.sendFile('index.html');
 });
 
-app.get('/adjective', function(req, res){
+app.get('/adjective', function(req, res) {
   res.json(getRandomWord(adjective));
 });
 
-app.get('/verb', function(req, res){
+app.get('/verb', function(req, res) {
   res.json(getRandomWord(verb));
 });
 
-app.get('/noun', function(req, res){
+app.get('/noun', function(req, res) {
   res.json(getRandomWord(noun));
 });
 
@@ -52,19 +38,19 @@ app.post('/adjective', function(req, res) {
   console.log(req.body);
   var word = postRandomWord(req.body.word, adjective);
   res.json(word);
-})
+});
 
 app.post('/verb', function(req, res) {
   console.log(req.body);
   var word = postRandomWord(req.body.word, verb);
   res.json(word);
-})
+});
 
 app.post('/noun', function(req, res) {
   console.log(req.body);
   var word = postRandomWord(req.body.word, noun);
   res.json(word);
-})
+});
 
 app.listen(port, function() {
   console.log('server available at http://localhost:' + port);
